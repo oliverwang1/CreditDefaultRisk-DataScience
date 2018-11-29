4843d4b34136a1cacc8d6afdbf91527abd36bcfa
osuhomebase <mbaron999@gmail.com>
2018-11-29 14:04:12 -0500

    added features to previous_application-Feature_Engineering.sql

--- previous_application-Feature_Engineering.sql
+++ previous_application-Feature_Engineering.sql
@@ -1,11 +1,11 @@
 update previous_application set amt_annuity = 0 where amt_annuity = ''
 update previous_application set amt_application = 0 where amt_application = ''
 
-select count(*), 
+select count(*) as total_prev_apps, 
 	avg(convert(decimal(9,2),amt_application)) as average_prev_app, 
 	avg(convert(decimal(9,2),amt_annuity)) as avg_prev_annuity, 
-	sk_id_curr 
-from previous_application 
+	sk_id_curr,
+	(SELECT top 1 days_Decision from previous_application p2 where p2.sk_id_curr = p1.sk_id_curr order by sk_id_prev desc) as days_since_last_decision,
+	(SELECT top 1 code_reject_reason from previous_application p2 where p2.sk_id_curr = p1.sk_id_curr order by sk_id_prev desc) as reject_reason
+from previous_application p1
 group by sk_id_curr
-
-select * from previous_application
