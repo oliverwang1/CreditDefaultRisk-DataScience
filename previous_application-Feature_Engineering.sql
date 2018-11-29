afe7a9d23df1c07dce695ee616368666d03c5607
osuhomebase <mbaron999@gmail.com>
2018-11-29 14:10:57 -0500

    updated previous_application-Feature_Engineering.sql so queries update application_Train instead of just ad hoc queries

--- previous_application-Feature_Engineering.sql
+++ previous_application-Feature_Engineering.sql
@@ -1,11 +1,20 @@
 update previous_application set amt_annuity = 0 where amt_annuity = ''
 update previous_application set amt_application = 0 where amt_application = ''
 
-select count(*) as total_prev_apps, 
+
+update app 
+set total_prev_apps = q.total_prev_apps,
+average_prev_app = q.average_prev_app,
+avg_prev_annuity = q.avg_prev_annuity,
+days_since_last_decision = q.days_since_last_decision,
+reject_reason = q.reject_reason
+from application_train app inner join 
+(select count(*) as total_prev_apps, 
 	avg(convert(decimal(9,2),amt_application)) as average_prev_app, 
 	avg(convert(decimal(9,2),amt_annuity)) as avg_prev_annuity, 
 	sk_id_curr,
 	(SELECT top 1 days_Decision from previous_application p2 where p2.sk_id_curr = p1.sk_id_curr order by sk_id_prev desc) as days_since_last_decision,
 	(SELECT top 1 code_reject_reason from previous_application p2 where p2.sk_id_curr = p1.sk_id_curr order by sk_id_prev desc) as reject_reason
 from previous_application p1
-group by sk_id_curr
+group by sk_id_curr) q 
+on app.SK_ID_CURR = q.sk_id_curr
